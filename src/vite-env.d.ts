0b1e78848f535da78bc6c1a15918b566b743d148
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2025-04-24 13:01:31 +0200

    More changes to decleration to try make deployment work

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,30 +1,3 @@
 /// <reference types="vite/client" />
 
-// CSS Modules
-declare module "*.module.css" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module "*.module.scss" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-// Static assets
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
-declare module "*.svg" {
-  import React from "react";
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
-}
-declare module "*.webp";
-
-// Global styles path
-declare module "@styles/*" {
-  const path: string;
-  export default path;
-}
+declare module "@styles/*";
