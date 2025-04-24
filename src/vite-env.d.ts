556428a48419ab6c6a185942c412afcbfd5eff4b
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2025-04-24 12:45:07 +0200

    change to fix deployment issues regarding styling

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 
 declare module "@styles/*";
+
+declare module "*.module.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.css" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
